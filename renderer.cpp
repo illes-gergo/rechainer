6b8c338de84654bb6e5581cf1cb8e77c3bca42de
Illés Gergő <illes.gergo1@gmail.com>
2025-01-04 11:41:42 +0100

    expanded the capabilities of 3d renderer

--- renderer.cpp
+++ renderer.cpp
@@ -1,8 +1,9 @@
 #include "./renderer.hpp"
 #include "qdirectionallight.h"
 #include "qentity.h"
+#include "qorbitcameracontroller.h"
 #include "qphongmaterial.h"
-#include "qpointlight.h"
+#include "qrandom.h"
 #include "qrgb.h"
 #include "qspheremesh.h"
 #include "qtransform.h"
@@ -11,14 +12,18 @@
 RenderWindow::RenderWindow() {
   scene = new Qt3DCore::QEntity();
   light = new Qt3DRender::QDirectionalLight(scene);
-  camController = new Qt3DExtras::QFirstPersonCameraController(scene);
+  camController = new Qt3DExtras::QOrbitCameraController(scene);
+  objects = new std::vector<Qt3DCore::QEntity *>();
+#ifdef DEBUG
+  randgen = new QRandomGenerator();
+#endif
 
   light->setIntensity(1);
   light->setWorldDirection(QVector3D(-1, -1, -1));
   cameraEntity = this->camera();
-  cameraEntity->lens()->setPerspectiveProjection(45.0f, 16.0f / 9.0f, 0.1f,
+  cameraEntity->lens()->setPerspectiveProjection(30.0f, 16.0f / 9.0f, 0.1f,
                                                  1000.0f);
-  cameraEntity->setPosition(QVector3D(0, 0, 20.0f));
+  cameraEntity->setPosition(QVector3D(1, 1, 1));
   cameraEntity->setUpVector(QVector3D(0, 1, 0));
   cameraEntity->setViewCenter(QVector3D(0, 0, 0));
   camController->setCamera(cameraEntity);
@@ -27,20 +32,25 @@ RenderWindow::RenderWindow() {
   atoms = new std::vector<Qt3DCore::QEntity>();
 }
 
-void RenderWindow::CreateSingleSphere() {
+void RenderWindow::CreateSingleSphere(float x, float y, float z, float radius) {
   auto *sphere = new Qt3DCore::QEntity(scene);
+  objects->push_back(sphere);
   auto *mesh = new Qt3DExtras::QSphereMesh;
   auto *transform = new Qt3DCore::QTransform();
   auto *material = new Qt3DExtras::QPhongMaterial();
   material->setDiffuse(QColor(QRgb(0xffffff)));
-  transform->setTranslation(QVector3D(0, 0, -5));
-  mesh->setRadius(3);
+  transform->setTranslation(QVector3D(x, y, z));
+  mesh->setRadius(radius);
   sphere->addComponent(mesh);
   sphere->addComponent(material);
   sphere->addComponent(transform);
 }
 
 void RenderWindow::viewScene() {
-  CreateSingleSphere();
+  #ifdef DEBUG
+  CreateSingleSphere(randgen->generateDouble()*10, randgen->generateDouble()*10,
+                     randgen->generateDouble()*10, randgen->generateDouble());
+  #endif
+
   this->show();
 }
--- renderer.hpp
+++ renderer.hpp
@@ -1,12 +1,19 @@
 #pragma once
 #include "qdirectionallight.h"
+#include "qentity.h"
 #include "qenvironmentlight.h"
+#include "qorbitcameracontroller.h"
 #include "qpointlight.h"
+#include "qrandom.h"
 #include <Qt3DCore>
 #include <Qt3DExtras>
 #include <Qt3DInput>
 #include <Qt3DRender>
 #include <vector>
+
+#ifndef DEBUG
+//#define DEBUG
+#endif
 
 class RenderWindow : public Qt3DExtras::Qt3DWindow {
 
@@ -15,11 +22,15 @@ private:
   std::vector<Qt3DCore::QEntity> *atoms;
   Qt3DRender::QDirectionalLight *light;
   Qt3DRender::QCamera *cameraEntity;
-  Qt3DExtras::QFirstPersonCameraController *camController;
+  Qt3DExtras::QOrbitCameraController *camController;
+  std::vector<Qt3DCore::QEntity *> *objects;
 
+#ifdef DEBUG
+  QRandomGenerator *randgen;
+#endif
 public:
   RenderWindow();
-  void CreateSingleSphere();
+  void CreateSingleSphere(float x, float y, float z, float radius);
 
 public slots:
   void viewScene();
