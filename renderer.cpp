d1d929495894a8644e74a7d69d764c25b3f53141
Illés Gergő <illes.gergo1@gmail.com>
2025-01-04 11:04:50 +0100

    renderer class, single spehere

--- renderer.cpp
+++ renderer.cpp
@@ -1,16 +1,43 @@
 #include "./renderer.hpp"
+#include "qdirectionallight.h"
 #include "qentity.h"
+#include "qphongmaterial.h"
+#include "qpointlight.h"
+#include "qrgb.h"
 #include "qspheremesh.h"
+#include "qtransform.h"
+#include <vector>
 
 RenderWindow::RenderWindow() {
   scene = new Qt3DCore::QEntity();
-  atoms = new std::vector<Qt3DCore::QEntity>();
+  light = new Qt3DRender::QDirectionalLight(scene);
+  camController = new Qt3DExtras::QFirstPersonCameraController(scene);
+
+  light->setIntensity(1);
+  light->setWorldDirection(QVector3D(-1, -1, -1));
+  cameraEntity = this->camera();
+  cameraEntity->lens()->setPerspectiveProjection(45.0f, 16.0f / 9.0f, 0.1f,
+                                                 1000.0f);
+  cameraEntity->setPosition(QVector3D(0, 0, 20.0f));
+  cameraEntity->setUpVector(QVector3D(0, 1, 0));
+  cameraEntity->setViewCenter(QVector3D(0, 0, 0));
+  camController->setCamera(cameraEntity);
   this->setRootEntity(scene);
+  this->defaultFrameGraph()->setClearColor(QColor(QRgb(0x4d4d4f)));
+  atoms = new std::vector<Qt3DCore::QEntity>();
 }
 
 void RenderWindow::CreateSingleSphere() {
-  Qt3DCore::QEntity *sphere = new Qt3DCore::QEntity(scene);
-  sphere->addComponent(new Qt3DExtras::QSphereMesh());
+  auto *sphere = new Qt3DCore::QEntity(scene);
+  auto *mesh = new Qt3DExtras::QSphereMesh;
+  auto *transform = new Qt3DCore::QTransform();
+  auto *material = new Qt3DExtras::QPhongMaterial();
+  material->setDiffuse(QColor(QRgb(0xffffff)));
+  transform->setTranslation(QVector3D(0, 0, -5));
+  mesh->setRadius(3);
+  sphere->addComponent(mesh);
+  sphere->addComponent(material);
+  sphere->addComponent(transform);
 }
 
 void RenderWindow::viewScene() {
--- renderer.hpp
+++ renderer.hpp
@@ -1,4 +1,7 @@
 #pragma once
+#include "qdirectionallight.h"
+#include "qenvironmentlight.h"
+#include "qpointlight.h"
 #include <Qt3DCore>
 #include <Qt3DExtras>
 #include <Qt3DInput>
@@ -10,6 +13,9 @@ class RenderWindow : public Qt3DExtras::Qt3DWindow {
 private:
   Qt3DCore::QEntity *scene;
   std::vector<Qt3DCore::QEntity> *atoms;
+  Qt3DRender::QDirectionalLight *light;
+  Qt3DRender::QCamera *cameraEntity;
+  Qt3DExtras::QFirstPersonCameraController *camController;
 
 public:
   RenderWindow();
